1cb4c2875ac4d2c66751a0267bc8083102c6de99
Paulo Massuci <paulo.massuci@actdigital.com>
2022-03-25 20:51:00 -0300

    Create Project

---
+++ Gist/Gist/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Gist
+//
+//  Created by Paulo Cesar Morandi Massuci on 25/03/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
